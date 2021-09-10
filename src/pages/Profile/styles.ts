e10f92dec9e1a198cc0171870b6d6853bca518d1
Icaro Cesar <icarorangel05@gmail.com>
2021-09-09 23:39:45 -0300

    styled profile

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,3 +1,24 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContainerProfiles = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+  flex-wrap: wrap;
+`;
+
+export const ContainerButtons = styled.div`
+  margin-bottom: 60px;
+  display: flex;
+  gap: 16px;
+`;

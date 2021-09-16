e74ca1119ee0a80b0f5f408ce47753246d4e701f
Icaro Cesar <icarorangel05@gmail.com>
2021-09-15 22:25:27 -0300

    add svgs and styled footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
-import { Container } from './styles';
-
+import { Container, ContainerInfos, ContainerSvgs } from './styles';
+import { ReactComponent as FacebookLogo } from '../../assets/facebook.svg';
+import { ReactComponent as InstagramLogo } from '../../assets/instagram.svg';
+import { ReactComponent as TwitterLogo } from '../../assets/twitter.svg';
+import { ReactComponent as YoutubeLogo } from '../../assets/youtube.svg';
+import Button from '../Button';
 const Footer = () => {
-  return <Container>somebady</Container>;
+  return (
+    <Container>
+      <ContainerSvgs>
+        <FacebookLogo />
+        <InstagramLogo />
+        <TwitterLogo />
+        <YoutubeLogo />
+      </ContainerSvgs>
+      <ContainerInfos>
+        <li>Idioma e legendas</li>
+        <li>Audiodescrição</li>
+        <li>Centro de ajuda</li>
+        <li>Cartão pré-pago</li>
+        <li>Imprensa</li>
+        <li>Relações com investidores</li>
+        <li>Carreiras</li>
+        <li>Termos de uso</li>
+        <li>Privacidade</li>
+        <li>Avisos Legais</li>
+        <li>Preferencias de cookies</li>
+        <li>Informações corporativas</li>
+        <li>Entre em contato</li>
+        <Button>Código do serviço</Button>
+        <span>© 1997-2021 Netflix, Inc.</span>
+      </ContainerInfos>
+    </Container>
+  );
 };
 
 export default Footer;
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,3 +1,41 @@
 import styled from 'styled-components';
 
-export const Container = styled.footer`ds`;
+export const Container = styled.footer`
+  padding: 80px 120px;
+`;
+
+export const ContainerSvgs = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 20px;
+  svg {
+    fill: #909090;
+    cursor: pointer;
+  }
+`;
+
+export const ContainerInfos = styled.ul`
+  margin-top: 10px;
+  height: 25vh;
+  li {
+    display: inline-flex;
+    text-align: left;
+    width: 230px;
+    list-style: none;
+    padding-bottom: 16px;
+    cursor: pointer;
+    color: #909090;
+    font-size: 13px;
+  }
+  button {
+    margin-top: 10px;
+    text-transform: none;
+    padding: 6px 4px;
+    font-size: 13px;
+  }
+  span {
+    font-size: 11px;
+    color: #909090;
+  }
+`;
